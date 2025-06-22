76902b234d99fb13b15cc0f3b7a51866c34873ca
shunm-999 <shunmiyazaki7812@gmail.com>
2025-06-22 19:29:11 +0900

    add lines test

--- src/line.rs
+++ src/line.rs
@@ -27,11 +27,52 @@ impl From<String> for Lines {
     }
 }
 
+impl From<&str> for Lines {
+    fn from(value: &str) -> Self {
+        let value = value.to_string();
+        Lines::from(value)
+    }
+}
+
 impl<'a> IntoIterator for &'a Lines {
     type Item = &'a Line;
     type IntoIter = slice::Iter<'a, Line>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.inner.iter()
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn split_empty_string() {
+        let string = String::from("");
+        let lines: Lines = string.into();
+
+        assert_eq!(lines.inner.len(), 1);
+        assert_eq!(lines.inner.first().unwrap().number, 1);
+        assert_eq!(lines.inner.first().unwrap().text, "");
+    }
+
+    #[test]
+    fn split_single_line_string() {
+        let string = String::from("a");
+        let lines: Lines = string.into();
+        assert_eq!(lines.inner.len(), 1);
+        assert_eq!(lines.inner.first().unwrap().number, 1);
+        assert_eq!(lines.inner.first().unwrap().text, "a");
+    }
+
+    #[test]
+    fn split_multiple_line_string() {
+        let string = String::from("a\nb");
+        let lines: Lines = string.into();
+        assert_eq!(lines.inner.len(), 2);
+        assert_eq!(lines.inner.first().unwrap().number, 1);
+        assert_eq!(lines.inner.first().unwrap().text, "a");
+        assert_eq!(lines.inner[1].number, 2);
+        assert_eq!(lines.inner[1].text, "b");
     }
 }
